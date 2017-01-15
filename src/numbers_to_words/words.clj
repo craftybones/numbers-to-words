143d5fefac10ea721aab5245eaa1a911ff6d1890
Srijayanth Sridhar <srijayanth@gmail.com>
2017-01-15 10:01:57 +0530

    Added words that represent numbers and associated tests

--- src/numbers_to_words/words.clj
+++ src/numbers_to_words/words.clj
@@ -1,65 +1,54 @@
 (ns numbers-to-words.words)
 
-(def tens {"one" "ten"
-           "two" "twenty"
-           "three" "thirty"
-           "four" "forty"
-           "five" "fifty"
-           "six" "sixty"
-           "seven" "seventy"
-           "eight" "eighty"
-           "nine" "ninety"})
+(def numbers-that-have-words
+  (concat (range 21)
+          (range 30 101 10)
+          [1000 1000000 1000000000]))
 
-(def units {0 "zero"
-            1 "one"
-            2 "two"
-            3 "three"
-            4 "four"
-            5 "five"
-            6 "six"
-            7 "seven"
-            8 "eight"
-            9 "nine"
-            10 "ten"
-            11 "eleven"
-            12 "twelve"
-            13 "thirteen"
-            14 "fourteen"
-            15 "fifteen"
-            16 "sixteen"
-            17 "seventeen"
-            18 "eighteen"
-            19 "nineteen"})
+(def words '("zero"
+             "one"
+             "two"
+             "three"
+             "four"
+             "five"
+             "six"
+             "seven"
+             "eight"
+             "nine"
+             "ten"
+             "eleven"
+             "twelve"
+             "thirteen"
+             "fourteen"
+             "fifteen"
+             "sixteen"
+             "seventeen"
+             "eighteen"
+             "nineteen"
+             "twenty"
+             "thirty"
+             "forty"
+             "fifty"
+             "sixty"
+             "seventy"
+             "eighty"
+             "ninety"
+             "hundred"
+             "thousand"
+             "million"
+             "billion"))
 
-(defn- in-thousands [msd]
-  [msd "thousand"])
+(def word-for-term (-> (zipmap numbers-that-have-words words)
+                       (assoc :and "and")))
 
-(defn- in-hundreds [msd]
-  [msd "hundred"])
+(def prefixes #{100 1000 1000000 1000000000})
 
-(defn- in-tens [msd]
-  (tens msd))
+(defn prefix-next-term [[terms prefix] next-term]
+  (cond
+    (contains? prefixes next-term) [(conj terms next-term) :and]
+    prefix [(conj terms :and next-term) nil]
+    :else [(conj terms next-term) nil]))
 
-(defn- in-units [msd]
-  (units msd))
+(def attach-prefix (comp first (partial reduce prefix-next-term [[] nil])))
 
-(def words {1 identity
-            10 in-tens
-            100 in-hundreds
-            1000 in-thousands})
-
-(defn prefix-for [term]
-  ({"hundred" "and"} term))
-
-(defn add-prefix
-  [[terms prefix] next-term]
-  (let [new-terms (if prefix
-                    (conj terms prefix next-term)
-                    (conj terms next-term))]
-    [new-terms (prefix-for next-term)]))
-
-(defn number-to-words [[msd mag :as msd-mag]]
-  (if (integer? msd)
-    [((words mag) (in-units msd))]
-    ((words mag) (map number-to-words msd))))
-
+(def in-words (comp (partial map word-for-term) attach-prefix))
--- test/numbers_to_words/words_test.clj
+++ test/numbers_to_words/words_test.clj
@@ -2,95 +2,60 @@
   (:require [clojure.test :refer :all]
             [numbers-to-words.words :refer :all]))
 
-(deftest num-to-words
-  (testing "units place"
-    (are [actual expected] (= actual (number-to-words expected))
-      ["zero"] [0 1]
-      ["one"] [1 1]
-      ["two"] [2 1]
-      ["three"] [3 1]
-      ["four"] [4 1]
-      ["five"] [5 1]
-      ["six"] [6 1]
-      ["seven"] [7 1]
-      ["eight"] [8 1]
-      ["nine"] [9 1]
-      ["ten"] [10 1]
-      ["eleven"] [11 1]
-      ["twelve"] [12 1]
-      ["thirteen"] [13 1]
-      ["fourteen"] [14 1]
-      ["fifteen"] [15 1]
-      ["sixteen"] [16 1]
-      ["seventeen"] [17 1]
-      ["eighteen"] [18 1]
-      ["nineteen"] [19 1]))
-  (testing "tens place"
-    (are [actual expected] (= actual (number-to-words expected))
-      ["ten"] [1 10]
-      ["twenty"] [2 10]
-      ["thirty"] [3 10]
-      ["forty"] [4 10]
-      ["fifty"] [5 10]
-      ["sixty"] [6 10]
-      ["seventy"] [7 10]
-      ["eighty"] [8 10]
-      ["ninety"] [9 10]))
-  (testing "hundreds place"
-    (are [actual expected] (= actual (number-to-words expected))
-      [["one" "hundred"]] [1 100]
-      [["two" "hundred"]] [2 100]
-      [["three" "hundred"]] [3 100]
-      [["four" "hundred"]] [4 100]
-      [["five" "hundred"]] [5 100]
-      [["six" "hundred"]] [6 100]
-      [["seven" "hundred"]] [7 100]
-      [["eight" "hundred"]] [8 100]
-      [["nine" "hundred"]] [9 100]))
-  (testing "thousands place"
-    (are [actual expected] (= actual (number-to-words expected))
-      [["one" "thousand"]] [1 1000]
-      [["two" "thousand"]] [2 1000]
-      [["three" "thousand"]] [3 1000]
-      [["four" "thousand"]] [4 1000]
-      [["five" "thousand"]] [5 1000]
-      [["six" "thousand"]] [6 1000]
-      [["seven" "thousand"]] [7 1000]
-      [["eight" "thousand"]] [8 1000]
-      [["nine" "thousand"]] [9 1000]
-      [["ten" "thousand"]] [10 1000]
-      [["eleven" "thousand"]] [11 1000]
-      [["twelve" "thousand"]] [12 1000]
-      [["thirteen" "thousand"]] [13 1000]
-      [["fourteen" "thousand"]] [14 1000]
-      [["fifteen" "thousand"]] [15 1000]
-      [["sixteen" "thousand"]] [16 1000]
-      [["seventeen" "thousand"]] [17 1000]
-      [["eighteen" "thousand"]] [18 1000]
-      [["nineteen" "thousand"]] [19 1000]))
-  (testing "thousands place when the msd is not just a unit number"
-    (are [actual expected] (= actual (number-to-words expected))
-      [[["twenty"]] "thousand"] [[[2 10]] 1000]
-      [[["twenty"] ["one"]] "thousand"] [[[2 10] [1 1]] 1000]
-      [[["twenty"] ["two"]] "thousand"] [[[2 10] [2 1]] 1000]
-      [[["twenty"] ["three"]] "thousand"] [[[2 10] [3 1]] 1000]
-      [[["twenty"] ["four"]] "thousand"] [[[2 10] [4 1]] 1000]
-      [[["twenty"] ["five"]] "thousand"] [[[2 10] [5 1]] 1000]
-      [[["twenty"] ["six"]] "thousand"] [[[2 10] [6 1]] 1000]
-      [[["twenty"] ["seven"]] "thousand"] [[[2 10] [7 1]] 1000]
-      [[["twenty"] ["eight"]] "thousand"] [[[2 10] [8 1]] 1000]
-      [[["twenty"] ["nine"]] "thousand"] [[[2 10] [9 1]] 1000])
-    (are [actual expected] (= actual (number-to-words expected))
-      [[[["one" "hundred"]]] "thousand"] [[[1 100]] 1000]
-      [[[["one" "hundred"]] ["one"]] "thousand"] [[[1 100] [1 1]] 1000]
-      [[[["one" "hundred"]] ["eleven"]] "thousand"] [[[1 100] [11 1]] 1000]
-      [[[["one" "hundred"]] ["twenty"]] "thousand"] [[[1 100] [2 10]] 1000]
-      [[[["one" "hundred"]] ["twenty"] ["five"]] "thousand"] [[[1 100] [2 10] [5 1]] 1000])))
+(deftest prefixing-next-term
+  (testing "prefixing when the next term is 100,1000 etc"
+    (is (= [[1 100] :and] (prefix-next-term [[1] nil] 100)))
+    (is (= [[1 100 :and 1] nil] (prefix-next-term [[1 100] :and] 1)))
+    (is (= [[1 1000] :and] (prefix-next-term [[1] nil] 1000)))
+    (is (= [[1 1000 :and 1] nil] (prefix-next-term [[1 1000] :and] 1)))
+    (is (= [[1 100 1000] :and] (prefix-next-term [[1 100] :and] 1000)))
+    (is (= [[1 100 1000 :and 1] nil] (prefix-next-term [[1 100 1000] :and] 1)))))
 
-(deftest adding-prefix
-  (testing "suggests a prefix for hundred"
-    (is (= [["one" "hundred"] "and"] (add-prefix [["one"] nil] "hundred"))))
-  (testing "adds a prefix when prefix present"
-    (is (= [["one" "hundred" "and" "one"] nil] (add-prefix [["one" "hundred"] "and"] "one"))))
-  (testing "does not add a prefix when prefix not present and does not suggest a prefix"
-    (is (= [["twenty" "three" "thousand"] nil] (add-prefix [["twenty" "three"] nil] "thousand")))))
+(deftest prefixing-with-and
+  (testing "cases with 100, 1000 etc"
+    (are [actual expected] (= (attach-prefix actual) expected)
+      [1 100] [1 100]
+      [1 100 1] [1 100 :and 1]
+      [1 100 20 3] [1 100 :and 20 3]
+      [5 1000] [5 1000]
+      [1 1000 1 100] [1 1000 :and 1 100] 
+      [5 1000 1] [5 1000 :and 1]
+      [5 1000 20 1] [5 1000 :and 20 1]
+      [5 1000 1 100] [5 1000 :and 1 100]
+      [5 1000 100 1] [5 1000 100 :and 1]
+      [1 100 1000 1] [1 100 1000 :and 1]
+      [1 100 1000 1 100] [1 100 1000 :and 1 100])))
+
+(deftest numbers-in-words
+  (testing "Assorted cases"
+    (are [actual expected] (= (in-words actual) expected)
+      [0] ["zero"]
+      [1] ["one"]
+      [2] ["two"]
+      [3] ["three"]
+      [4] ["four"]
+      [5] ["five"]
+      [6] ["six"]
+      [7] ["seven"]
+      [8] ["eight"]
+      [9] ["nine"]
+      [10] ["ten"]
+      [11] ["eleven"]
+      [12] ["twelve"]
+      [13] ["thirteen"]
+      [14] ["fourteen"]
+      [15] ["fifteen"]
+      [16] ["sixteen"]
+      [17] ["seventeen"]
+      [18] ["eighteen"]
+      [19] ["nineteen"]
+      [20] ["twenty"]
+      [20 1] ["twenty" "one"]
+      [30 2] ["thirty" "two"]
+      [40 3] ["forty" "three"]
+      [50 4] ["fifty" "four"]
+      [60 5] ["sixty" "five"]
+      [70 6] ["seventy" "six"]
+      [80 7] ["eighty" "seven"]
+      [90 8] ["ninety" "eight"]
+      [1 100] ["one" "hundred"])))
